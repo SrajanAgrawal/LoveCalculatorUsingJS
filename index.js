27a4e2d0ec08397dbd56369ffb7dd2d77f9afacc
Srajan Agrawal <91273821+SrajanAgrawal@users.noreply.github.com>
2022-05-26 18:40:53 +0530

    final commit

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+// love calculator api 
+class LOVE{
+  getResult(firstName, secondName){
+      return new Promise( (resolve, reject) => {     
+          fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${firstName}&fname=${secondName}`, {
+              "headers": {"x-rapidapi-key": "0988c3b4admsh7cc82e6e92882eap19c2abjsnb4ed3302c211"}
+          })
+          .then(response => response.json())
+          .then(result => resolve(result))
+          .catch(err => reject(err));
+      })
+  }
+}
 var checkLove = document.getElementById('checkLove');
 var heading = document.getElementById('heading');
 var inputs = document.querySelectorAll('input');
 lovePercentage = parseInt(Math.random() * 100 + 1);
+
+const love = new LOVE();
+
+
+
 checkLove.addEventListener('click' , function() {
-  if(!(inputs[0].value == '' || inputs[1].value == '')) {
-  lovePercentage = parseInt(Math.random() * 100 + 1);
-  heading.innerHTML = "You Both Are " + lovePercentage + "%" + " Compatible";
+  var firstName = inputs[0].value.toUpperCase();
+  var secondName = inputs[1].value.toUpperCase();
+  if(!(firstName == '' || secondName == '')) {
+  // lovePercentage = parseInt(Math.random() * 100 + 1);
+  console.log("up till here success !! ");
+  heading.innerHTML = '.......Loading result......';
+  heading.style.fontSize = '24px';
+  love.getResult(firstName, secondName)
+  .then(response => 
+    heading.innerHTML = response.fname + " & " + response.sname + " Are <b>" +  response.percentage + "%" + "</b> Compatible")
+    inputs[0].value='';
+    inputs[1].value='';
 }
 else{
   heading.innerHTML = "Enter Your Name First"
 }
 
 })
+
